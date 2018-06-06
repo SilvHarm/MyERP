5e84e574117c04c6517a7e6466459dde8d9189e9
SilvHarm <silvharm@gmail.com>
2018-06-06 15:11:25 +0200

    Fixed compile problem from previous commit

--- src/myerp-consumer/src/test-consumer/java/com/dummy/myerp/consumer/dao/impl/db/dao/ComptabiliteDaoImplITCase.java
+++ src/myerp-consumer/src/test-consumer/java/com/dummy/myerp/consumer/dao/impl/db/dao/ComptabiliteDaoImplITCase.java
@@ -5,8 +5,8 @@ import java.util.List;
 import org.junit.Test;
 import org.springframework.util.Assert;
 
-import com.dummy.myerp.model.bean.comptabilite.CompteComptable
-import com.dummy.myerp.consumer.dao.impl.db.dao.ComptabiliteDaoImpl;;
+import com.dummy.myerp.model.bean.comptabilite.CompteComptable;
+import com.dummy.myerp.consumer.dao.impl.db.dao.ComptabiliteDaoImpl;
 
 public class ComptabiliteDaoImplITCase {
 	
